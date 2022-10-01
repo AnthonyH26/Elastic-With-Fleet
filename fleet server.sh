2cbf58a277ac91db8f8f23af70573e42945eadae
AnthonyH26 <37834038+AnthonyH26@users.noreply.github.com>
2022-10-01 23:09:18 +0100

    Updated Fleet Server installation

--- fleet server.sh
+++ fleet server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Please ensure you have set the default settings for Elasticsearch in the web UI first"
 sudo apt install elastic-agent -y
-sudo /usr/share/elasticsearch/bin/elasticsearch-certutil ca --ca-dn "cn=Fleet CA" --pem -s --out /tmp/fleet_ca.zip
+sudo /usr/share/elasticsearch/bin/elasticsearch-certutil ca --pem -s --out /tmp/fleet_ca.zip
 sudo mkdir /usr/share/elasticsearch/certs/fleet -p
 sudo unzip /tmp/fleet_ca.zip -d /usr/share/elasticsearch/certs/fleet
 echo 'Please insert if you want 1.) IP address 2.) DNS entry, 3.) Both for fleet server certificates'
@@ -49,7 +49,8 @@ echo "${INSTANCES_NON_SEPERATE}" > /tmp/fleet_instances.yml
 fi
 sudo /usr/share/elasticsearch/bin/elasticsearch-certutil cert --ca-cert /usr/share/elasticsearch/certs/fleet/ca/ca.crt --ca-key /usr/share/elasticsearch/certs/fleet/ca/ca.key --pem -s --in /tmp/fleet_instances.yml --out /tmp/fleet_server.zip
 sudo unzip /tmp/fleet_server.zip -d /usr/share/elasticsearch/certs/fleet
-
+sudo cp /usr/share/elasticsearch/certs/fleet/ca/ca.crt /usr/local/share/ca-certificates/elastic-fleet.crt
+sudo update-ca-certificates
 echo "Please enter the fleet server that was also entered into the Web UI including the port and http/https"
 read -p ">> Fleet Server Address: " fleetAddress
 echo "Please enter the Elasticsearch Address entered into the Web UI including the port and http/https"
